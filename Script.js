9feb9d0f32aadcb325cb2f78998ff18733eb0bb7
gloriakzd <gloriarubab.a@gmail.com>
2026-02-16 23:51:12 +0000

    Added images and edits

--- Script.js
+++ Script.js
@@ -471,3 +471,21 @@ requestAnimationFrame(() => {
 });
 
 });
+
+
+/* ==============================
+   Fscroll inside the window = sections gently appear.
+============================== */
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+},{
+  threshold: 0.15
+});
+
+document.querySelectorAll('.reveal').forEach(el=>{
+  observer.observe(el);
+});
